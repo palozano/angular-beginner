213a6191bfb8a6d9901ba1145ea491cb3f7bc1dd
Pablo Lozano <palozano@math.uc3m.es>
2019-06-30 15:06:05 +0200

    exam section 1

--- proyecto-inicial/app.js
+++ proyecto-inicial/app.js
@@ -1,16 +1,49 @@
 "use strict";
-var Avenger = /** @class */ (function () {
-    function Avenger(nombre, equipo, nombreReal) {
-        this.nombre = "Sin nombre";
-        this.equipo = undefined;
-        this.nombreReal = undefined;
-        this.puedePelar = false;
-        this.peleasGanadas = 0;
-        this.nombre = nombre;
-        this.equipo = equipo;
-        this.nombreReal = nombreReal;
+// Uso de let y Const
+var nombre = "Ricardo Tapia";
+var edad = 23;
+var PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+};
+var batman = {
+    nombre: "Bruno Díaz",
+    artesMarciales: ["Karate", "Aikido", "Wing Chun", "Jiu-Jitsu"]
+};
+// Convertir esta funcion a una funcion de flecha
+// function resultadoDoble( a, b ){
+//   return (a + b) * 2
+// }
+var resultadoDoble = function (a, b) { return (a + b) * 2; };
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger(nombre, poder, arma) {
+    if (arma === void 0) { arma = "arco"; }
+    var mensaje;
+    if (poder) {
+        // mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+        mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
     }
-    return Avenger;
+    else {
+        // mensaje = nombre + " tiene un " + poder
+        mensaje = nombre + " tiene un poder " + poder;
+    }
+}
+;
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+var Rectagulo = /** @class */ (function () {
+    function Rectagulo() {
+    }
+    Rectagulo.prototype.calcularArea = function () {
+        return this.base * this.altura;
+    };
+    return Rectagulo;
 }());
-var antman = new Avenger("Antman", "Capi", "Scott Lang");
-console.log(antman);
--- proyecto-inicial/app.ts
+++ proyecto-inicial/app.ts
@@ -1,23 +1,68 @@
+// Uso de let y Const
+let nombre:string = "Ricardo Tapia";
+let edad:number = 23;
+
+const PERSONAJE : { nombre:string, edad:number } = {
+  nombre: nombre,
+  edad: edad
+};
 
 
+// Cree una interfaz que sirva para validar el siguiente objeto
 
-class Avenger {
+interface Batman {
+  nombre:string;
+  artesMarciales: string[];
+}
 
-  nombre: string = "Sin nombre";
-  equipo: string = undefined;
-  nombreReal: string = undefined;
-
-  puedePelar: boolean = false;
-  peleasGanadas: number = 0;
-
-  constructor( nombre:string, equipo:string, nombreReal:string ){
-    this.nombre = nombre;
-    this.equipo = equipo;
-    this.nombreReal = nombreReal;
-  }
+let batman:Batman = {
+  nombre: "Bruno Díaz",
+  artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
 }
 
 
-let antman:Avenger = new Avenger( "Antman", "Capi", "Scott Lang" );
+// Convertir esta funcion a una funcion de flecha
+// function resultadoDoble( a, b ){
+//   return (a + b) * 2
+// }
 
-console.log(antman);
+let resultadoDoble = (a:number, b:number ) => (a+b)*2;
+
+
+
+
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger( nombre:string, poder?:string, arma:string="arco" ){
+  let mensaje:string;
+
+  if( poder ){
+     // mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+     mensaje = `${ nombre } tiene el poder de: ${ poder } y un arma: ${ arma }`;
+  }else{
+     // mensaje = nombre + " tiene un " + poder
+     mensaje = `${ nombre } tiene un poder ${ poder }`;
+  }
+};
+
+
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+
+
+class Rectagulo {
+  base:number;
+  altura:number;
+
+  calcularArea():number {
+    return this.base*this.altura;
+  }
+
+}
